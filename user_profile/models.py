ac06f7a3730b1e40d38a20873ea254ea5ffcbf90
nate-hunter <hunter.j.nate@gmail.com>
2023-10-06 19:25:24 -0400

    defined `UserProfile` model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  first_name = models.CharField(max_length=155, blank=True)
+  last_name = models.CharField(max_length=155, blank=True)
+  phone_number_1 = models.CharField(max_length=20, blank=True)
+  address_city = models.CharField(max_length=155, blank=True)
+
+  def __str__(self):
+      return f'{self.first_name} {self.last_name}'
+
