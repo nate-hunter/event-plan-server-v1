2bb0f05be69bc800c425e470a344d5b54242114d
nate-hunter <hunter.j.nate@gmail.com>
2023-10-06 18:19:06 -0400

    updated `app` settings with `user_profile` + drf auth config

--- app/settings.py
+++ app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'something',
+    'user_profile',
 ]
 
 MIDDLEWARE = [
@@ -149,3 +150,12 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
