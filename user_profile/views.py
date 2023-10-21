8af255f708fd87a2f9cc51ade55e490d714c3c37
nate-hunter <hunter.j.nate@gmail.com>
2023-10-21 13:49:47 -0400

    created `GetUserProfileView`

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+
+class GetUserProfileView(APIView):
+  def get(self, request, format=None):
+    user = User.objects.get(id=self.request.user.id)
+    print(f'\n[request user] {self.request.user}', f'\n[user] {user}')
+
+    user_profile = UserProfile.objects.get(user=user)
+    serializer = UserProfileSerializer(user_profile)
+
+    return Response(serializer.data)
+
